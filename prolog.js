c3907de6c4b2057fd2b0effcd7f462773545183e
Danylo Treshchov <danylo.treshchov@mycit.ie>
2024-06-16 19:57:05 +0100

    Implemented basic story generator

--- prolog.js
+++ prolog.js
@@ -1,14 +1,12 @@
-function runTalespin() {
-    const outputElement = document.getElementById("output");
-    outputElement.textContent = '';  // Clear previous output
+const outputElement = document.getElementById("output");
+outputElement.textContent = '';  // Clear previous output
 
-    const session = pl.create(1000);
+const session = pl.create(1000);
 
-    const query = `consult('tales.pl'), init(Tale).`;
-    session.query(query);
+const query = `consult('tales.pl'), init(Tale).`;
+session.query(query);
 
-    session.answer(result => {
-        let tale = result.lookup('Tale');
-        outputElement.textContent += tale + '\n';
-    });
-}
+session.answer(result => {
+    let tale = result.lookup('Tale');
+    outputElement.textContent += tale + '\n';
+});
